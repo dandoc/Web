45de0731867200b904730e918141ba43f212c8f2
dandoc <dandoc3370@gmail.com>
2021-05-28 21:06:30 +0900

    template literal(node)

---
+++ Node.js/syntax/template.js
@@ -0,0 +1,10 @@
+var name = 'Lee'
+var letter = 'Dear '+name+'\nasdfsfwe wgiqoghqwoigq wgweog '+name+' ih '+name+' wiofjsd ewnwekfnjkf '+name+' oife '+name+' sqwfoqw hui fasdoifhofawh jun afiowajfaw dan oejfoqwij doc';
+
+console.log(letter);
+
+var letter = `Dear ${name}
+
+asdfsfwe wgiqoghqwoigq wgweog ${name} ih ${name} wiofjsd ewnwekfnjkf ${name} sqwfoqw hui fasdoifhofawh jun afiowajfaw dan oejfoqwij doc`;
+
+console.log(letter);
