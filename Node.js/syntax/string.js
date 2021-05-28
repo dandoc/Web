ad8718e5dff28c5bae3dae70faef7fdd917336c1
dandoc <dandoc3370@gmail.com>
2021-05-28 20:56:45 +0900

    string, number data type, varialbe(node)

---
+++ Node.js/syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('Hello! I am a programmer and also student.'.length);
---
+++ Node.js/syntax/variable2.js
@@ -0,0 +1,5 @@
+var name = 'Lee'
+var letter = 'Dear '+name+' asdfsfwe wgiqoghqwoigq wgweog '+name+' ih '+name+' wiofjsd ewnwekfnjkf '+name+' oife '+name+' sqwfoqw hui fasdoifhofawh jun afiowajfaw dan oejfoqwij doc';
+
+console.log(letter);
+console.log(1+1);
