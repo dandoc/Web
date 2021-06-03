c67dabfe17faf64778e555707f32994f3f6f27ac
dandoc <dandoc3370@gmail.com>
2021-06-03 10:00:41 +0900

    Boolean, Conditional syntax-if (nodejs)

--- Node.js/main.js
+++ Node.js/main.js
@@ -5,38 +5,71 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title = "Welcome"
+    var pathname = url.parse(_url, true).pathname;
+    
+    
+    console.log(url.parse(_url, true).pathname);
+    
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, decription){
+          var title = 'Welcome';
+          var decription = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset=" utf-8">
+            </head>
+      
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>${decription}</p>
+            </body>
+          </html> 
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else{
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, decription){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset=" utf-8">
+            </head>
+      
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>${decription}</p>
+            </body>
+          </html> 
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+      }
+    }else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, decription){
-      var template = `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset=" utf-8">
-        </head>
-  
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${decription}</p>
-        </body>
-      </html> 
-      `;
-      response.end(template);
-    })
   
    
 });
